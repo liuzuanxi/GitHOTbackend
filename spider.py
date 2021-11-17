2a3f6951e9527c48904c97263b1eb1fad5bbe294
SkySpaceF <65939170+SkySpaceF@users.noreply.github.com>
2021-11-17 22:03:20 +0800

    Add files via upload
    增加默认参数，提升代码鲁棒性

--- spider.py
+++ spider.py
@@ -10,16 +10,14 @@ import urllib
 from urllib.request import urlopen, Request
 import requests
 from scrapy.selector import Selector
-import re
+import time
+
 
 # TODO 4.实现爬虫类
 class Spider():
 
     def __init__(self):
         '''爬虫静态配置'''
-
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'}
 
     def _parse(self, url):
 
@@ -29,38 +27,88 @@ class Spider():
         :return data: 爬取内容
         '''
 
-        response = requests.get(url, headers=self.headers)
-        dev
-        # html = response.text
-        s = Selector(text = html)
-        title = s.xpath('//span[@itemprop="name"]/a/text()').extract_first()
+        html = self.getHtml(url)
+        s = Selector(text=html)
+        title = s.xpath('//strong[@itemprop="name"]/a/text()').extract_first()
+        title = title if title else ''
+        # print('Title: '+title)
+        introductions = s.xpath('//div[@class="BorderGrid-cell"]/p[1]/text()').extract()
+        introduction = ''
 
-        html = response.text
-        s = Selector(text = html)
-        title = s.xpath('//span[@class="author flex-self-stretch"]').extract()
-        dev
+        for item in introductions:
+            introduction += item.strip()
+        introduction = introduction if introduction else ''
+        # print('Introduction: '+introduction.strip())
+        keyword_list = s.xpath('//div[@class="f6"]/a/text()').extract()
 
+        print('Keyword:')
+        keywords = []
+        for keyword in keyword_list:
+            keyword = keyword.strip()
+            keywords.append(keyword)
+            # print(keyword)
 
-
-
-
-
-
-
+        star = s.xpath(
+            '//*[@id="repository-container-header"]//li/div/a[@class="social-count js-social-count"]/text()').extract_first()
+        star = star.strip() if star else ''
+        if star[-1] == 'k':
+            star = int(star)*1000
+        else:
+            star = int(star)
+        # print('Stars: ',star)
+        # print()
         '''your demo'''
-        data = {"title": str, "url": url, "introduction": str,'stars':int,"keyWords":list(str)}
+        data = {"title": title, "url": url, "introduction": introduction, 'stars': star, "keyWords": keywords}
 
         '''end'''
         return data
 
-    def _getAllproject(self, num=10000):
+    def getHtml(self, url):
+        proxies = {
+            "http": "http://47.243.226.217:14467",
+            "http": "http://47.243.226.217:14465",
+            "http": "http://47.243.226.217:14466",
+            "http": "http://47.242.190.60:10808",
+            "http": "http://47.242.66.236:5329",
+        }
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'}
+        try:
+            response = requests.get(url, headers=headers, proxies=proxies)
+            html = response.text
+        except Exception as err:
+            print(err)
+        return html
+
+    def _getAllproject(self, num = 50):
         '''
             获得所有项目中一定量的项目
         :param num:获取项目数量
         :return project_urls: 返回链接列表
         '''
         '''your demo'''
-        project_urls = list(str)
+        project_urls = []
+        count = 0
+        url = 'https://hub.fastgit.org/search?p={}&q=stars%3A%3E100&type=Repositories'
+        for page in range(1, 100):
+            current_url = url.format(page)
+            # print('current url:  ',current_url)
+            html = self.getHtml(current_url)
+            s = Selector(text=html)
+            urls = s.xpath('//li/div[2]/div[1]/div/a/@href').extract()
+            # '//*[@id="js-pjax-container"]/div/div[3]/div/ul/li[10]/div[2]/div[1]/div/a'
+            pre_url = 'https://hub.fastgit.org'
+            for item in urls:
+                project_url = pre_url + item
+                if len(project_urls) >= num:
+                    break
+                project_urls.append(project_url)
+            print(len(project_urls))
+            if len(project_urls) >= num:
+                break
+            time.sleep(5)
+            # print('end')
+            # print()
 
         '''end'''
         return project_urls
